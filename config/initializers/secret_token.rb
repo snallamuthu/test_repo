98afad93f41f2025451ffb1d98aaf807edfd7ac0
snallamuthu <sangeeth.nallamuthu@listertechnologies.com>
2013-07-16 14:13:01 +0530

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TestRepo::Application.config.secret_token = 'd2b24af53c99fa377da1bfbb10780743f53321f9493b96ab9c6e80b01fbea6fad0d72d3d12d2d9d0fa6e8ac1d20d7e69d55544cdf38346c5a12c5f035f8a44da'
